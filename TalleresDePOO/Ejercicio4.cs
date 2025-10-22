d141b4fe61103f947376d211f71321dacb07d666
cano0102 <canoanderson418@gmail.com>
2025-10-22 02:11:52 -0500

    Add Libro and Biblioteca classes with methods for adding and listing books

---
+++ TalleresDePOO/Ejercicio4.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace MiProyecto.App
+
+{
+
+
+    internal class Libro
+    {
+        public string Titulo { get; set; }
+        public string Editorial { get; set; }
+        public string Autor { get; set; }
+
+        public int AnioPublicacion { get; set; }
+
+
+        public Libro(string Titulo, string Editorial, string Autor,int AnioPublicacion)
+        {
+            this.Titulo = Titulo;
+            this.Editorial = Editorial;
+            this.Autor = Autor;
+            this.AnioPublicacion = AnioPublicacion;
+
+        }
+
+
+    }
+
+    internal class Biblioteca
+    {
+        public List<Libro> Libros = new List<Libro>();
+        public void AgregarLibro()
+        {
+
+            Console.WriteLine("Dame el nombre del libro :");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Dame el nombre del libro :");
+            string editorial = Console.ReadLine();
+
+
+            Console.WriteLine("Dame el nombre del libro :");
+            string autor = Console.ReadLine();
+
+
+            Console.WriteLine("Dame el nombre del libro :");
+            int anioPublicacion = int.Parse(Console.ReadLine());
+
+
+            Libro CaracteristicasDelLibro = new Libro(nombre, editorial, autor, anioPublicacion);
+
+            Libros.Add(CaracteristicasDelLibro);
+
+
+             
+            
+
+
+        }
+
+        public void ListarLibros()
+        {
+            foreach (var LibroListado in Libros)
+            {
+                Console.WriteLine($"NOMBRE {LibroListado.Titulo} Editorial:{LibroListado.Editorial} Autor: {LibroListado.Autor} Anio {LibroListado.AnioPublicacion} ");
+            }
+        }
+        
+        
+    }
+}
